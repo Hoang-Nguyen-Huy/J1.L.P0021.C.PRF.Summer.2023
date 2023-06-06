342c70c8d4dc4efefd41e1c59eb9d7e9698993a1
Hoang Nguyen <hoangmegamind1302@gmail.com>
2023-06-06 16:41:42 +0700

    Deadline tuan 10

---
+++ BigHomeWork.c
@@ -0,0 +1,13 @@
+/*
+Name: Nguyen Huy Hoang
+Class: SE1809
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include <windows.h>
+#include <unistd.h>
+
